c15d24b233e605689c42f0e0e03d043f5186ff63
Alejandro Daas <aledaas@gmail.com>
2026-05-22 15:22:20 -0300

    fix: auto dismiss global feedback message

--- shared/src/commonMain/kotlin/com/aledaas/compose_app_starter/App.kt
+++ shared/src/commonMain/kotlin/com/aledaas/compose_app_starter/App.kt
@@ -1,8 +1,10 @@
 package com.aledaas.compose_app_starter
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -10,44 +12,41 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.aledaas.compose_app_starter.core.auth.AuthState
 import com.aledaas.compose_app_starter.core.components.AppSnackbarHost
+import com.aledaas.compose_app_starter.core.designsystem.AppSpacing
 import com.aledaas.compose_app_starter.core.designsystem.AppTheme
 import com.aledaas.compose_app_starter.core.di.AppContainer
 import com.aledaas.compose_app_starter.core.navigation.AppNavigation
 import com.aledaas.compose_app_starter.modules.auth.presentation.LoginScreen
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import androidx.compose.foundation.layout.fillMaxSize
-import com.aledaas.compose_app_starter.core.designsystem.AppSpacing
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
-
         val authState by AppContainer.authController.authState
         val loginUiState by AppContainer.authController.loginUiState
         val feedbackMessage by AppContainer.feedbackController.currentMessage
-
         val coroutineScope = rememberCoroutineScope()
+
+        LaunchedEffect(feedbackMessage) {
+            if (feedbackMessage != null) {
+                delay(3000)
+                AppContainer.feedbackController.clear()
+            }
+        }
 
         Box(
             modifier = Modifier.fillMaxSize()
         ) {
-
             when (authState) {
-
                 AuthState.Unknown,
                 AuthState.Unauthenticated -> {
-
                     LoginScreen(
                         uiState = loginUiState,
                         onSignIn = { email, password ->
-
                             coroutineScope.launch {
-
-                                AppContainer.authController.signIn(
-                                    email,
-                                    password
-                                )
+                                AppContainer.authController.signIn(email, password)
 
                                 AppContainer.feedbackController.show(
                                     message = "Successfully signed in"
@@ -63,7 +62,6 @@ fun App() {
             }
 
             feedbackMessage?.let { message ->
-
                 Box(
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
